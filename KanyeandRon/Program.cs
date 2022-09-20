18fa46c5b62ff45c4a33077b618eb515e214d348
juliusdc14 <juliusdc05@gmail.com>
2022-09-20 09:14:44 -0500

    working wit JSON

---
+++ KanyeandRon/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeandRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quotes.KanyeQuote();
+
+            Quotes.RonQuote();
+            
+        }
+
+        
+    }
+}
+
---
+++ KanyeandRon/Quotes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeandRon
+{
+    public class Quotes
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"_________");
+            Console.WriteLine($"Kanye: '{kanyeQuote}'");
+            Console.WriteLine($"___________");
+        }
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"_________");
+            Console.WriteLine($"Ron: '{ronQuote}'");
+            Console.WriteLine($"___________");
+        }
+    }
+}
+
